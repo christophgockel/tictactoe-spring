63550432cf101e2499d7e2238514e66f20992a65
Christoph Gockel <github@christophgockel.de>
2014-10-29 09:36:40 +0000

    Add game mode and board size selection

--- src/main/java/de/christophgockel/tictactoe/spring/IndexController.java
+++ src/main/java/de/christophgockel/tictactoe/spring/IndexController.java
@@ -1,6 +1,9 @@
 package de.christophgockel.tictactoe.spring;
 
+import de.christophgockel.tictactoe.game.Board;
+import de.christophgockel.tictactoe.game.PlayerPairsFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -8,7 +11,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Controller
 public class IndexController {
   @RequestMapping(value = "/", method = GET)
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("modes", PlayerPairsFactory.getAvailablePairs());
+    model.addAttribute("sizes", Board.getAvailableSizes());
+
     return "index";
   }
 }
